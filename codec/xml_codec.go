eef711964b47b27c9f216c370a3e4ec817224c32
nandagopalan <3970199+nandagopalan@users.noreply.github.com>
2022-12-20 11:16:12 -0500

    moved the json,xml and yaml codec to ReaderWriter implementations only Updated the BaseCodec to check for validate on read and validate before  write to keep it in a common place Added getDefaultCodec function

--- codec/xml_codec.go
+++ codec/xml_codec.go
@@ -8,19 +8,10 @@ import (
 	"io/ioutil"
 )
 
-type XMLCodec struct {
-	options map[string]interface{}
+type xmlRW struct {
 }
 
-func NewXmlCodec(options map[string]interface{}) Codec {
-	return BaseCodec{readerWriter: XmlRW(options)}
-}
-
-func XmlRW(options map[string]interface{}) *XMLCodec {
-	return &XMLCodec{options: options}
-}
-
-func (x *XMLCodec) Write(v interface{}, w io.Writer) error {
+func (x *xmlRW) Write(v interface{}, w io.Writer) error {
 	output, err := xml.Marshal(v)
 	if err != nil {
 		return errors.New(fmt.Sprintf("xml marshal error: %d", err))
@@ -32,7 +23,7 @@ func (x *XMLCodec) Write(v interface{}, w io.Writer) error {
 	return nil
 }
 
-func (x *XMLCodec) Read(r io.Reader, v interface{}) error {
+func (x *xmlRW) Read(r io.Reader, v interface{}) error {
 	b, err := ioutil.ReadAll(r)
 	if err != nil {
 		return errors.New(fmt.Sprintf("xml input error: %d", err))
