af85fa15b585e7c00a6c13b12d313cbdc57fe398
nandagopalan <3970199+nandagopalan@users.noreply.github.com>
2022-12-23 10:28:14 -0600

    store and manager interface added

---
+++ secrets/store.go
@@ -0,0 +1,32 @@
+package secrets
+
+import (
+	"context"
+	"sync"
+)
+
+type Store interface {
+	Get(key string, ctx context.Context) (*Credential, error)
+	Provider() string
+}
+
+type Manager struct {
+	stores map[string]Store
+	once   sync.Once
+}
+
+func (m *Manager) Register(store Store) {
+	if m.stores == nil {
+		m.once.Do(func() {
+			m.stores = make(map[string]Store)
+		})
+	}
+	m.stores[store.Provider()] = store
+}
+
+func (m *Manager) Store(name string) (store Store) {
+	if m.stores != nil {
+		store = m.stores[name]
+	}
+	return
+}
