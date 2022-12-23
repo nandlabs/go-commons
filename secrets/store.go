26f7a3e1fca28f05bc86457c3b79da4eb91aa8ad
nandagopalan <3970199+nandagopalan@users.noreply.github.com>
2022-12-23 10:53:39 -0600

    Added write method to the Store interface

--- secrets/store.go
+++ secrets/store.go
@@ -7,6 +7,7 @@ import (
 
 type Store interface {
 	Get(key string, ctx context.Context) (*Credential, error)
+	Write(key string, credential *Credential, ctx context.Context) error
 	Provider() string
 }
 
