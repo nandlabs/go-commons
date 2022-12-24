c44cec3a76ba0c6f75176652311af27aec2a645e
nandagopalan <3970199+nandagopalan@users.noreply.github.com>
2022-12-24 07:23:27 -0600

    moved manager implementation to store_manager

---
+++ secrets/storemanager.go
@@ -0,0 +1,24 @@
+package secrets
+
+import "sync"
+
+type Manager struct {
+	stores map[string]Store
+	once   sync.Once
+}
+
+func (m *Manager) Register(store Store) {
+	if m.stores == nil {
+		m.once.Do(func() {
+			m.stores = make(map[string]Store)
+		})
+	}
+	m.stores[store.Provider()] = store
+}
+
+func (m *Manager) Store(name string) (store Store) {
+	if m.stores != nil {
+		store = m.stores[name]
+	}
+	return
+}
