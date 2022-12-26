94b7a0f2696277b4b55509d55abd609fd4d48ea5
nandagopalan <3970199+nandagopalan@users.noreply.github.com>
2022-12-26 09:18:46 -0600

    Updaed Doco and added Type Func

--- secrets/credential.go
+++ secrets/credential.go
@@ -1,23 +1,50 @@
 package secrets
 
 import (
+	"go.nandlabs.io/commons/textutils"
 	"time"
 
 	"go.nandlabs.io/commons/config"
 )
 
+const (
+	CredType   = "CredType"
+	TextSecret = "TextSecret"
+)
+
+//Credential Struct that can represent any secret value.
+//Fields
+//
+// Value is an array of bytes to hold decrypted value of a secret.
+
+// LastUpdated specifies the last updated time of the credential.
+//
+// Version string specifies the current version of the credential. If the credential type/Store implementation
+// maintains multiple version the previous version info can be maintained in metadata field.
+//
+// MetaData Any additional key,value attributes that needs to be associated with the credentials can be done using this
+//property
+
 type Credential struct {
 	Value       []byte
 	LastUpdated time.Time
 	Version     string
-	MetaData    config.Properties
+	MetaData    *config.Properties
 }
 
+// Str function gets the Credential.Value field as string
 func (c *Credential) Str() (s string) {
-
 	if c.Value != nil {
 		s = string(c.Value)
 	}
+	return
+}
 
+//Type Returns the type of the credential
+func (c *Credential) Type() (s string) {
+	s = TextSecret
+	if c.MetaData != nil {
+		s = c.MetaData.Get(CredType, textutils.EmptyStr)
+	}
 	return
 }
