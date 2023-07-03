b9f7836429629a72a1475d71448cc2977924251a
Aditya Kumar <adityak007mr@gmail.com>
2023-07-03 23:21:29 +0530

    added test cases

--- clients/messaging/local_message_test.go
+++ clients/messaging/local_message_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestLocalMessage_SetBodyBytes(t *testing.T) {
-	message := LocalMessage{
+	message := &LocalMessage{
 		headers:     make(map[string]interface{}),
 		headerTypes: make(map[string]reflect.Kind),
 		body:        &bytes.Buffer{},
@@ -25,7 +25,7 @@ func TestLocalMessage_SetBodyBytes(t *testing.T) {
 }
 
 func TestLocalMessage_SetBodyStr(t *testing.T) {
-	message := LocalMessage{
+	message := &LocalMessage{
 		headers:     make(map[string]interface{}),
 		headerTypes: make(map[string]reflect.Kind),
 		body:        &bytes.Buffer{},
@@ -42,7 +42,7 @@ func TestLocalMessage_SetBodyStr(t *testing.T) {
 }
 
 func TestLocalMessage_SetFrom(t *testing.T) {
-	message := LocalMessage{
+	message := &LocalMessage{
 		headers:     make(map[string]interface{}),
 		headerTypes: make(map[string]reflect.Kind),
 		body:        &bytes.Buffer{},
@@ -61,7 +61,7 @@ func TestLocalMessage_SetFrom(t *testing.T) {
 }
 
 func TestLocalMessage_WriteJSON(t *testing.T) {
-	message := LocalMessage{
+	message := &LocalMessage{
 		headers:     make(map[string]interface{}),
 		headerTypes: make(map[string]reflect.Kind),
 		body:        &bytes.Buffer{},
@@ -79,7 +79,7 @@ func TestLocalMessage_WriteJSON(t *testing.T) {
 }
 
 //func TestLocalMessage_WriteXML(t *testing.T) {
-//	message := LocalMessage{
+//	message := &LocalMessage{
 //		headers:     make(map[string]interface{}),
 //		headerTypes: make(map[string]reflect.Kind),
 //		body:        &bytes.Buffer{},
@@ -96,3 +96,153 @@ func TestLocalMessage_WriteJSON(t *testing.T) {
 //		t.Errorf("Error ReadXML: %v", err)
 //	}
 //}
+
+func TestLocalMessage_Header(t *testing.T) {
+	message := &LocalMessage{
+		headers:     make(map[string]interface{}),
+		headerTypes: make(map[string]reflect.Kind),
+		body:        &bytes.Buffer{},
+	}
+	message.SetHeader("key", []byte("test"))
+	got, exists := message.GetHeader("key")
+	if exists {
+		if string(got) != "test" {
+			t.Errorf("Error Header Setters/Getters, got : %v, want : test", got)
+		}
+	}
+}
+
+func TestLocalMessage_BoolHeader(t *testing.T) {
+	message := &LocalMessage{
+		headers:     make(map[string]interface{}),
+		headerTypes: make(map[string]reflect.Kind),
+		body:        &bytes.Buffer{},
+	}
+	message.SetBoolHeader("isPresent", true)
+	got, exists := message.GetBoolHeader("isPresent")
+	if exists {
+		if got != true {
+			t.Errorf("Error BoolHeader Setters/Getters, got : %v", got)
+		}
+	}
+}
+
+func TestLocalMessage_StrHeader(t *testing.T) {
+	message := &LocalMessage{
+		headers:     make(map[string]interface{}),
+		headerTypes: make(map[string]reflect.Kind),
+		body:        &bytes.Buffer{},
+	}
+	message.SetStrHeader("header-obj", "header-value")
+	got, exists := message.GetStrHeader("header-obj")
+	if exists {
+		if got != "header-value" {
+			t.Errorf("Error BoolHeader Setters/Getters, got : %v, want : header-value", got)
+		}
+	}
+}
+
+func TestLocalMessage_IntHeader(t *testing.T) {
+	message := &LocalMessage{
+		headers:     make(map[string]interface{}),
+		headerTypes: make(map[string]reflect.Kind),
+		body:        &bytes.Buffer{},
+	}
+	message.SetIntHeader("header-obj", 10)
+	got, exists := message.GetIntHeader("header-obj")
+	if exists {
+		if got != 10 {
+			t.Errorf("Error BoolHeader Setters/Getters, got : %v, want : 10", got)
+		}
+	}
+}
+
+func TestLocalMessage_Int8Header(t *testing.T) {
+	message := &LocalMessage{
+		headers:     make(map[string]interface{}),
+		headerTypes: make(map[string]reflect.Kind),
+		body:        &bytes.Buffer{},
+	}
+	message.SetInt8Header("header-obj", 120)
+	got, exists := message.GetInt8Header("header-obj")
+	if exists {
+		if got != 120 {
+			t.Errorf("Error BoolHeader Setters/Getters, got : %v, want : 120", got)
+		}
+	}
+}
+
+func TestLocalMessage_Int16Header(t *testing.T) {
+	message := &LocalMessage{
+		headers:     make(map[string]interface{}),
+		headerTypes: make(map[string]reflect.Kind),
+		body:        &bytes.Buffer{},
+	}
+	message.SetInt16Header("header-obj", 32767)
+	got, exists := message.GetInt16Header("header-obj")
+	if exists {
+		if got != 32767 {
+			t.Errorf("Error BoolHeader Setters/Getters, got : %v, want : 120", got)
+		}
+	}
+}
+
+func TestLocalMessage_Int32Header(t *testing.T) {
+	message := &LocalMessage{
+		headers:     make(map[string]interface{}),
+		headerTypes: make(map[string]reflect.Kind),
+		body:        &bytes.Buffer{},
+	}
+	message.SetInt32Header("header-obj", 2147483647)
+	got, exists := message.GetInt32Header("header-obj")
+	if exists {
+		if got != 2147483647 {
+			t.Errorf("Error BoolHeader Setters/Getters, got : %v, want : 2147483647", got)
+		}
+	}
+}
+
+func TestLocalMessage_Int64Header(t *testing.T) {
+	message := &LocalMessage{
+		headers:     make(map[string]interface{}),
+		headerTypes: make(map[string]reflect.Kind),
+		body:        &bytes.Buffer{},
+	}
+	message.SetInt64Header("header-obj", 9223372036854775807)
+	got, exists := message.GetInt64Header("header-obj")
+	if exists {
+		if got != 9223372036854775807 {
+			t.Errorf("Error BoolHeader Setters/Getters, got : %v, want : 9223372036854775807", got)
+		}
+	}
+}
+
+func TestLocalMessage_Float32Header(t *testing.T) {
+	message := &LocalMessage{
+		headers:     make(map[string]interface{}),
+		headerTypes: make(map[string]reflect.Kind),
+		body:        &bytes.Buffer{},
+	}
+	message.SetFloatHeader("header-obj", 200.0)
+	got, exists := message.GetFloatHeader("header-obj")
+	if exists {
+		if got != 200.0 {
+			t.Errorf("Error BoolHeader Setters/Getters, got : %v, want : 200.0", got)
+		}
+	}
+}
+
+func TestLocalMessage_Float64Header(t *testing.T) {
+	message := &LocalMessage{
+		headers:     make(map[string]interface{}),
+		headerTypes: make(map[string]reflect.Kind),
+		body:        &bytes.Buffer{},
+	}
+	message.SetFloat64Header("header-obj", -1.7e+308)
+	got, exists := message.GetFloat64Header("header-obj")
+	if exists {
+		if got != -1.7e+308 {
+			t.Errorf("Error BoolHeader Setters/Getters, got : %v, want : -1.7E+308", got)
+		}
+	}
+}
--- clients/messaging/local_provider_test.go
+++ clients/messaging/local_provider_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestLocalMessagingSystem_Send(t *testing.T) {
+func TestLocalProvider_Send(t *testing.T) {
 	lms := NewMessaging()
 	msg := &LocalMessage{
 		headers:     make(map[string]interface{}),
@@ -21,6 +21,40 @@ func TestLocalMessagingSystem_Send(t *testing.T) {
 	}
 	uri, _ := url.Parse("chan://localhost:8080")
 	got := lms.Send(uri, msg)
+	if got != nil {
+		t.Errorf("Error got :: %v", got)
+	}
+
+	uriErr, _ := url.Parse("http://localhost:8080")
+	got = lms.Send(uriErr, msg)
+	if got.Error() != "unsupported provider with url http://localhost:8080" {
+		t.Errorf("Error got :: %v", got)
+	}
+}
+func TestLocalProvider_SendBatch(t *testing.T) {
+	lms := NewMessaging()
+	msg1 := &LocalMessage{
+		headers:     make(map[string]interface{}),
+		headerTypes: make(map[string]reflect.Kind),
+		body:        &bytes.Buffer{},
+	}
+	input1 := "this is a test string 1"
+	_, err := msg1.SetBodyStr(input1)
+	if err != nil {
+		t.Errorf("Error SetBodyStr:: %v", err)
+	}
+	msg2 := &LocalMessage{
+		headers:     make(map[string]interface{}),
+		headerTypes: make(map[string]reflect.Kind),
+		body:        &bytes.Buffer{},
+	}
+	input2 := "this is a test string 2"
+	_, err = msg2.SetBodyStr(input2)
+	if err != nil {
+		t.Errorf("Error SetBodyStr:: %v", err)
+	}
+	uri, _ := url.Parse("chan://localhost:8080")
+	got := lms.SendBatch(uri, msg1, msg2)
 	if got != nil {
 		t.Errorf("Error got :: %v", got)
 	}
